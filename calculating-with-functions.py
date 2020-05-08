22603c53d8feaf80991f4d2258ddd8ff3f2a433e
Jorge Luiz <jorgeluizjk@gmail.com>
2020-05-08 02:40:15 -0300

    kyu 5 code.

---
+++ calculating-with-functions.py
@@ -0,0 +1,37 @@
+'''
+This time we want to write calculations using functions and get the results. Let's have a look at some examples:
+
+seven(times(five())) # must return 35
+four(plus(nine())) # must return 13
+eight(minus(three())) # must return 5
+six(divided_by(two())) # must return 3
+
+Requirements:
+
+    There must be a function for each number from 0 ("zero") to 9 ("nine")
+    There must be a function for each of the following mathematical operations: plus, minus, times, dividedBy (divided_by in Ruby and Python)
+    Each calculation consist of exactly one operation and two numbers
+    The most outer function represents the left operand, the most inner function represents the right operand
+    Divison should be integer division. For example, this should return 2, not 2.666666...:
+
+eight(divided_by(three()))
+
+'''
+
+def zero(operation=None): return 0 if not operation else operation(0)
+def one(operation=None): return 1 if not operation else  operation(1)
+def two(operation=None): return 2 if not operation else operation(2)
+def three(operation=None): return 3 if not operation else operation(3)
+def four(operation=None): return 4 if not operation else operation(4)
+def five(operation=None): return 5 if not operation else operation(5)
+def six(operation=None): return 6 if not operation else operation(6)
+def seven(operation=None): return 7 if not operation else operation(7)
+def eight(operation=None): return 8 if not operation else operation(8)
+def nine(operation=None): return 9 if not operation else operation(9)
+
+def plus(b): return lambda a: a + b
+def minus(b): return lambda a: a - b
+def times(b): return lambda a: a * b
+def divided_by(b): return lambda a: int(a / b) if b != 0 else 'Imposible divide by zero!'
+
+print(two(plus(three())))
